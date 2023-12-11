7305ade15776ca0d5d77794972c9b337df90bfbf
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2023-12-11 13:18:19 +0100

    bariera działa, nie ma duplikatów

--- lab9/pierwsze.py
+++ lab9/pierwsze.py
@@ -2,23 +2,6 @@ import math
 import threading
 
 
-class Barrier:
-    def __init__(self, num_threads):
-        self.num_threads = num_threads
-        self.count = 0
-        self.mutex = threading.Lock()
-        self.barrier = threading.Condition(self.mutex)
-
-    def wait(self):
-        with self.mutex:
-            self.count += 1
-            if self.count == self.num_threads:
-                self.count = 0
-                self.barrier.notify_all()
-            else:
-                self.barrier.wait()
-
-
 def isPrimary(numb):
     s = math.ceil(math.sqrt(numb))
     for i in range(2, s+1):
@@ -27,39 +10,38 @@ def isPrimary(numb):
     return True
 
 
-def find_primary(l, r, barrier):
+def find_primary(l, r):
     primary_numbers = []
     for i in range(l, r+1):
         if isPrimary(i):
             primary_numbers.append(i)
-    barrier.wait()
     return primary_numbers
 
 
-def find_primary_in_sublist(l, r, barrier, result_list):
-    partial_primary = find_primary(l, r, barrier)
-    result_list.extend(partial_primary)
+def find_primary_in_sublist(l, r, barrier, result_list, result_lock):
+    print("początek pracy " + threading.current_thread().name)
+    partial_primary = find_primary(l, r)
+    with result_lock:
+        result_list.extend(partial_primary)
+    barrier.wait()
+    print("koniec czekania " + threading.current_thread().name)
 
 
-def parallel_primary_finding(l, r, num_threads):
+def parallel_primary_finding(l, r, num_threads, barrier):
     chunk_size = math.floor((r-l+1) / num_threads)
-    list_len = r-l+1
     partial_primary = []
-
-    barrier = Barrier(num_threads)
-
-    threads = []
+    result_lock = threading.Lock()
+    print("początek pracy")
     for i in range(num_threads):
         start = l + i * chunk_size
         end = l + (i + 1) * chunk_size if i < num_threads - 1 else r
         thread = threading.Thread(target=find_primary_in_sublist, args=(
-            start, end, barrier, partial_primary))
+            start, end, barrier, partial_primary, result_lock))
         thread.start()
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
-
+    print("koniec pracy")
+    barrier.wait()
+    print("koniec czekania")
+    partial_primary = list(set(partial_primary))  # usuwanie duplikatów
     partial_primary.sort()
     return partial_primary
 
@@ -67,9 +49,10 @@ def parallel_primary_finding(l, r, num_threads):
 if __name__ == "__main__":
     l = 2
     r = 20
-    num_threads = 4
+    num_threads = 5
+    bar = threading.Barrier(num_threads+1)
 
-    result = parallel_primary_finding(l, r, num_threads)
+    result = parallel_primary_finding(l, r, num_threads, bar)
 
     # test
     print(result)
--- threadingPB.py
+++ threadingPB.py
@@ -1,25 +1,26 @@
 # utworzenie dwóch wątków współbieżnych wykonującyh
-# funkcję f 
+# funkcję f
 # synchronizacja barierą
 import threading
 import time
 
-def f(arg,name,s,bar):
+
+def f(arg, name, s, bar):
     for i in range(arg):
-        print(name,'i=',i)
+        print(name, 'i=', i)
         time.sleep(s)
-    print ("koniec pracy "+name)
+    print("koniec pracy "+name)
     bar.wait()
-    print ("koniec czekania "+name)
+    print("koniec czekania "+name)
+
 
 b = threading.Barrier(3)
-t1 = threading.Thread(target = f, args = (5,'t1',1,b))
-t2 = threading.Thread(target = f, args = (2,'t2',0.7,b))
+t1 = threading.Thread(target=f, args=(5, 't1', 1, b))
+t2 = threading.Thread(target=f, args=(2, 't2', 0.7, b))
 t1.start()
 t2.start()
-print ("koniec pracy ")
+print("koniec pracy ")
 b.wait()
 # t1.join()
 # t2.join()
-print ("koniec czekania")
-
+print("koniec czekania")
